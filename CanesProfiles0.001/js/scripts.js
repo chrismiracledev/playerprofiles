b99c898180eed345bc7ee4da8c99f99a4354dd65
chrismiracledev <94769016+chrismiracledev@users.noreply.github.com>
2021-11-22 12:56:20 -0500

    Add files via upload

--- CanesProfiles0.001/js/scripts.js
+++ CanesProfiles0.001/js/scripts.js
@@ -1,36 +1,117 @@
-console.log("1");
-function getPlayers() {
-    console.log("2");
-    var request = new XMLHttpRequest()
+const api_url = 'https://statsapi.web.nhl.com/api/v1/teams/12/roster'
+async function getPlayers() {
+  const response = await fetch(api_url);
+  const data = await response.json();
+  console.log(data);
+  var card = ''
+  var i = 0;
+  data.roster.forEach(player => {
+    const { id, fullName, link } = data.roster[i].person;
+    const jerseyNumber = data.roster[i].jerseyNumber;
+    const { code, name, type, abbreviation } = data.roster[i].position;
+    console.log(jerseyNumber)
+    console.log(fullName)
+    console.log(link)
+    console.log(id)
 
-    request.open('GET', 'https://statsapi.web.nhl.com/api/v1/teams/12/roster', true)
-    request.onload = function() {
-        //Begin accessing JSON Data here
-        var data = JSON.parse(this.response)
-        if (request.status >= 200 && request.status < 400) {
-            var card = ''
-            var i = 0;
-            data.roster.forEach(player => {
-                
-                console.log(i);
-                console.log(player);
-                //getPlayer(player.person.id)
+    var card__button__id = 'card__button__' + player.person.id
 
-                var card__button__id = 'card__button__' + player.person.id
+                card = card + `
+                  <div class="col-12 col-lg-6 col-xl-4 my-1 my-lg-3">
+                    <button class="card-button" id="card__button__${id}">
+                      <div class="card" id="${id}">
+                        <div class="card-block">
+                          <div class="row">
+                            <div class="col-5">
+                              <img class="img-fluid" src="images/CanesHeadshots/${fullName}Headshot.jpg" alt="${fullName}Headshot">
+                            </div> <!-- col -->
+                            <div class="col-7">
+                              <div class="card-body">
+                                <h6 class="card-title">${fullName} - ${player.position.abbreviation}</h6>
+                                <p class="card-text"></p>
+                              </div> <!-- card body -->
+                            </div> <!-- col -->
+                          </div>
+                        </div>
+                      </div>
+                    </button>
+                  </div>
+                `;
 
-                card = card + '<div class="col-12 col-lg-6 col-xl-4 my-1 my-lg-3"><button class="card-button" id="' + card__button__id + '"><div class="card"><div class="card-block"><div class="row"><div class="col-5"><img class="img-fluid" src="images/CanesHeadshots/' + player.person.fullName + 'Headshot.jpg" alt="' + player.person.fullName + 'Headshot"></div> <!-- col --><div class="col-7"><div class="card-body"><h6 class="card-title">' + player.person.fullName + ' - ' + player.position.abbreviation + '</h6><p class="card-text">' + player.position.name + ' - ' + player.jerseyNumber + '</p></div> <!-- card body --></div> <!-- col --></div></div></div></button></div>'
-                
-                i++;
-            })
-            document.getElementById("player__cards").innerHTML = card
-        } else {
-            console.log('error');
-        }
-    }
-
-    request.send()
+    i++;
+  })
+  document.querySelector("#player__cards").innerHTML = card;
 }
 
+
+// fetch('https://statsapi.web.nhl.com/api/v1/teams/12/roster')
+//   .then(response => {
+//     return response.json();
+//   })
+//   .then(players => {
+//     console.log(players);
+//   });
+
+
+
+
+// console.log("1");
+// function getPlayers() {
+//     console.log("2");
+//     var request = new XMLHttpRequest()
+
+//     request.open('GET', 'https://statsapi.web.nhl.com/api/v1/teams/12/roster', true)
+//     request.onload = function() {
+//         //Begin accessing JSON Data here
+//         var data = JSON.parse(this.response)
+//         if (request.status >= 200 && request.status < 400) {
+//             var card = ''
+//             var i = 0;
+//             data.roster.forEach(player => {
+                
+//                 console.log(i);
+//                 console.log(player);
+//                 //getPlayer(player.person.id)
+
+//                 var card__button__id = 'card__button__' + player.person.id
+
+//                 card = card + `
+//                   <div class="col-12 col-lg-6 col-xl-4 my-1 my-lg-3">
+//                     <button class="card-button" id="${card__button__id}">
+//                       <div class="card" id="${player.person.id}">
+//                         <div class="card-block">
+//                           <div class="row">
+//                             <div class="col-5">
+//                               <img class="img-fluid" src="images/CanesHeadshots/${player.person.fullName}Headshot.jpg" alt="${player.person.fullName}Headshot">
+//                             </div> <!-- col -->
+//                             <div class="col-7">
+//                               <div class="card-body">
+//                                 <h6 class="card-title">${player.person.fullName} - ${player.position.abbreviation}</h6>
+//                                 <p class="card-text"></p>
+//                               </div> <!-- card body -->
+//                             </div> <!-- col -->
+//                           </div>
+//                         </div>
+//                       </div>
+//                     </button>
+//                   </div>
+//                 `;
+
+//                 i++;
+                  
+//             })
+
+            
+//             document.querySelector("#player__cards").innerHTML = card;
+//         } else {
+//             console.log('error');
+//         }
+//     }
+
+//     request.send()
+// }
+
+// document.querySelectorAll(".card-text").forEach(item => console.log("3"))
 
 // Get the modal
 var modal = document.getElementById("card__modal");
